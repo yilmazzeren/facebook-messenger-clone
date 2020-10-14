b5bcae38e6e9644122a5bbce316f3feb49f02c91
yilmazzeren <56710444+yilmazzeren@users.noreply.github.com>
2020-10-15 02:49:34 +0300

    Add material-ui and some changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Button ,FormControl,InputLabel,Input} from '@material-ui/core';
 import "./App.css";
+import Message from "./Message";
 
 function App() {
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState(["hello", "hi", "whats up"]);
 
-  console.log(input);
-  console.log(messages);
+  const [messages, setMessages] = useState([
+    {username : "Yılmaz" , text : "Hey"},
+    {username : "Özlem" , text : "Hey Yılmaz"}]);
+
+  const [username,setUsername] = useState('');
+
+
   const sendMessage = (event) => {
     event.preventDefault()
-    setMessages([...messages, input]);
+    setMessages([...messages, {username : username , text : input}]);
     setInput("");
   };
+
+  useEffect (() => {
+    setUsername(prompt("Please enter your name"))
+    
+  },[])
 
   return (
     <div className="App">
       <h1>Hello</h1>
-      
-      <FormControl>
-        <InputLabel >Enter a message...</InputLabel>
-        <Input value={input} onChange={(event) => setInput(event.target.value)} />
-        <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>Send Message</Button>
-      </FormControl>
-      
-      
-
+      <h2>Welcome {username}</h2>
+      <form >
+        <FormControl>
+          <InputLabel >Enter a message...</InputLabel>
+          <Input value={input} onChange={(event) => setInput(event.target.value)} />
+          <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>Send Message</Button>
+        </FormControl>
+      </form>
       {
         messages.map((message,key) => (
-        <p key={key}>{message}</p>
+          <Message key={key} username = {message.username} text={message.text} />
         ))
       }
     </div>
---
+++ src/Message.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./Message.css";
+
+function Message(props) {
+  return (
+    <Card className="message">
+      <CardContent>
+        <Typography color="white" variant="h5" component="h2">
+          {props.username} : {props.text}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Message;
